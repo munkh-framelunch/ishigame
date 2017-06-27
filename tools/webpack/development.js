8ca25e5b9e12f96bd19b2c1f20680925ade0ef44
kaave <abe@framelunch.jp>
2017-06-27 17:37:33 +0900

    [Project] webpackにもNODE_ENVを反映

--- tools/webpack/development.js
+++ tools/webpack/development.js
@@ -5,6 +5,7 @@ export default Object.assign({}, base, {
   cache: true,
   devtool: 'inline-source-map',
   plugins: [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': "'development'" }),
     new webpack.LoaderOptionsPlugin({ debug: false }),
     new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
   ]
